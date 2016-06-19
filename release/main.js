92577eb4d5b4d9b40717f3fdf23592fcd75df70b
Simon Situ <ssitu001@gmail.com>
2016-06-19 12:15:20 -0700

    added regex validation for phone number

--- release/main.js
+++ release/main.js
@@ -14,9 +14,19 @@ kbValues.currentTime = currentDate.getFullYear() + '-' +
 
 var userName = prompt("Hello, what's your name?");
 
-document.body.innerHTML = '<h1>Hello ' + userName + ' !</h1>' + 
-                          '<p>' + kbValues.projectName + ' ' + kbValues.versionNumber +
-                          ' viewed on: ' + kbValues.currentTime + '</p>';
+var phoneNumber = prompt("Hello, " + userName + " what's your phone number?");
 
+var phoneNumberPattern = /1?-?\(?\d{3}[\-\)]\d{3}-\d{4}/;
+
+var output = '<h1>Hello, ' + userName + '!</h1>';
+
+if (phoneNumberPattern.test(phoneNumber)) {
+  output =  output + '<p>' + kbValues.projectName + ' ' + kbValues.versionNumber +
+                          ' viewed on: ' + kbValues.currentTime + '</p>';
+} else {
+  output = output + '<h2>That number is invalid: ' + phoneNumber + '</h2>';
+}
+
+document.body.innerHTML = output;
 
 var images = document.querySelectorAll('div.userContentWrapper img');
